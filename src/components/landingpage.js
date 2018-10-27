78c35009dbfb0fbdb5a550b6de0bf0b0c6debd3a
Yuhuan Fan <yfan4@binghamton.edu>
2018-10-26 22:39:13 -0700

    Adding tabs to project page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
   render() {
     return(
-      <div>
-        <h1>Landing Page</h1>
+      <div style = {{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://cdn-images-1.medium.com/max/1200/1*RpxcLZjkK2fZYECYCBeYUA.jpeg"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h1>Software Engineer</h1>
+              <hr/>
+              <p className="skill-list">Java / JavaScript</p>
+              <div className="social-links">
+                {/*Github*/}
+                <a href ="https://github.com/Kate9202" target="_blank" rel="noopener noreferrer">
+                  <i class="fa fa-github-square" aria-hidden="true" />
+                </a>
+
+                {/*Linkedin*/}
+                <a href ="https://www.linkedin.com/in/yfan4/" target="_blank" rel="noopener noreferrer">
+                  <i class="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+
+                {/*Instagram*/}
+                <a href ="https://www.instagram.com/yuhuan_f/" target="_blank" rel="noopener noreferrer">
+                  <i class="fa fa-instagram" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     )
   }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,13 +1,102 @@
 import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, CardMenu, Button, IconButton } from 'react-mdl';
 
-class Project extends Component {
+class Projects extends Component {
+  constructor(props){
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories(){
+    if(this.state.activeTab === 0){
+      return(
+        <div className="projects-grid">
+          {/*Project 1*/}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color:"white", height:'176px', background: 'url(https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png) center / cover'}}>React Project 1</CardTitle>
+            <CardText>
+              This eBook is for the use of anyone anywhere at no cost and with almost no restrictions whatsoever.
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color:'#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/*Project 2*/}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color:"white", height:'176px', background: 'url(https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png) center / cover'}}>React Project 2</CardTitle>
+            <CardText>
+              This eBook is for the use of anyone anywhere at no cost and with almost no restrictions whatsoever.
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color:'#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/*Project 3*/}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color:"white", height:'176px', background: 'url(https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png) center / cover'}}>React Project 3</CardTitle>
+            <CardText>
+              This eBook is for the use of anyone anywhere at no cost and with almost no restrictions whatsoever.
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color:'#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+
+
+
+      )
+    } else if(this.state.activeTab === 1){
+      return(
+        <div><h1>This is Angular</h1></div>
+      )
+    } else if(this.state.activeTab === 2){
+      return(
+        <div><h1>This is VueJS</h1></div>
+      )
+    } else if(this.state.activeTab === 3){
+      return(
+        <div><h1>This is MongoDB</h1></div>
+      )
+    }
+  }
+
+
   render() {
     return(
-      <div>
-        <h1>Project Page</h1>
+      <div className="category-tabs">
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+          <Tab>React</Tab>
+          <Tab>Angular</Tab>
+          <Tab>VueJs</Tab>
+          <Tab>MongoDB</Tab>
+        </Tabs>
+
+        <Grid >
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
       </div>
     )
   }
 }
 
-export default Project;
+export default Projects;
